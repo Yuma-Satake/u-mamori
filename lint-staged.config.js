92e2dcde5ff5187341afe4d9729a73cd19532764
Yuma Satake <satous.y@icloud.com>
2024-07-16 09:27:21 +0900

    fix: commit lint 削除

--- lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  '**/*.{js,jsx,ts,tsx}': [
-    'eslint --fix --ext .js,.jsx,.ts,.tsx --report-unused-disable-directives --max-warnings 0',
-    'prettier --write',
-    'git add',
-  ],
-  '**/*.{html,css,json,yml,yaml,md}': ['prettier --write', 'git add'],
-};
