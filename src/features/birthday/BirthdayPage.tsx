611ddf56fb1c8800d26e56b17e749f1b7ba7799d
Yuma Satake <satous.y@icloud.com>
2024-09-10 23:31:51 +0900

    fix: 誕生日ページ修正

--- src/features/birthday/BirthdayPage.tsx
+++ src/features/birthday/BirthdayPage.tsx
@@ -5,45 +5,72 @@ import { FC, useEffect, useState } from 'react';
 export const BirthdayPage: FC = () => {
   const maxImgNumber = 3;
   const [targetImageNumber, setTargetImageNumber] = useState(0);
+  const [prevImageNumber, setPrevImageNumber] = useState(0);
+  const [isFading, setIsFading] = useState(false); // フェード状態の管理
 
   useEffect(() => {
     const interval = setInterval(() => {
+      setIsFading(true); // フェード開始
+      setTimeout(() => {
+        setPrevImageNumber(targetImageNumber); // 現在の画像番号を保存
+      }, 1000);
       setTargetImageNumber((prev) => {
         if (prev + 1 === maxImgNumber) return 0;
         return prev + 1;
       });
+      setTimeout(() => {
+        setIsFading(false); // フェード終了
+      }, 5000); // フェードが終わった1秒後に状態を更新
     }, 5000);
     return () => clearInterval(interval);
-  }, []);
+  });
 
   return (
     <>
-      <button
-        type="button"
-        onClick={() => {
-          setTargetImageNumber((prev) => {
-            if (prev + 1 === maxImgNumber) return 0;
-            return prev + 1;
-          });
-        }}
-        style={{
-          border: 'none',
+      <Stack
+        sx={{
+          height: '100dvh',
+          width: '100dvw',
         }}
       >
-        <Stack
-          sx={{
-            height: '100dvh',
-            width: '100dvw',
+        {/* 新しい画像 */}
+        <div
+          style={{
+            zIndex: 1,
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
             backgroundImage: `url(/momo/momo_${targetImageNumber}.png)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center',
-            transition: 'background-image 1s ease-in-out',
+            transition: 'opacity 1s ease-in-out', // フェードイン効果
+            opacity: isFading ? 1 : 0, // フェードが始まったら新しい画像をフェードイン
           }}
         />
-      </button>
+
+        {/* 前の画像 */}
+        <div
+          style={{
+            zIndex: 0,
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundImage: `url(/momo/momo_${prevImageNumber}.png)`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            // opacity: isFading ? 0 : 1, // フェードが始まったら前の画像をフェードアウト
+          }}
+        />
+      </Stack>
+
       <Typography
         variant="caption"
         sx={{
+          zIndex: 2,
           position: 'fixed',
           bottom: 10,
           right: 15,
